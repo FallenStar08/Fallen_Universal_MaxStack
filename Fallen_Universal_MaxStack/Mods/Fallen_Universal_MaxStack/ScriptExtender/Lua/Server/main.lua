b91d2585acbfcfcb587569720b39107a943fa8ad
FallenStar <lasalle.joffrey@gmail.com>
2023-11-19 20:44:48 +0100

    Also patch local templates, rainbow text

--- Fallen_Universal_MaxStack/Mods/Fallen_Universal_MaxStack/ScriptExtender/Lua/Server/main.lua
+++ Fallen_Universal_MaxStack/Mods/Fallen_Universal_MaxStack/ScriptExtender/Lua/Server/main.lua
@@ -1,7 +1,7 @@
 Ext.Require("Shared/_Globals.lua")
 Ext.Require("Shared/_Utils.lua")
 _G.MaxStackAmount = 99999999
-_=nil
+
 
 --Just In Case...
 Ext.Osiris.RegisterListener("TemplateAddedTo", 4, "after", function(root, item, inventoryHolder, addType)
@@ -16,14 +16,20 @@ end)
 Ext.Osiris.RegisterListener("LevelGameplayStarted", 2, "after", function(level, isEditorMode)
     local startTime = Ext.Utils.MonotonicTime()
     local templates = Ext.Template.GetAllRootTemplates()
+    local localTemplates = Ext.Template.GetAllLocalTemplates()
     for k, v in pairs(templates) do
+        if v.TemplateType == "item" and v.InventoryType == 0 then
+            v.MaxStackAmount = _G.MaxStackAmount
+        end
+    end
+    for k, v in pairs(localTemplates) do
         if v.TemplateType == "item" and v.InventoryType == 0 then
             v.MaxStackAmount = _G.MaxStackAmount
         end
     end
     local endTime = Ext.Utils.MonotonicTime()
     local elapsedTime = endTime - startTime
-    BasicPrint("Templates MaxStackAmount modified in " .. elapsedTime .. " milliseconds!",_,_,"Fall_MaxStack")
+    BasicPrint("Templates MaxStackAmount modified in " .. elapsedTime .. " milliseconds!",_,_,"Fall_MaxStack",true)
 end)
 
 
