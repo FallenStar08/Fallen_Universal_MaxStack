01e00e1eedfc6eaac63a276bd0b7b47ec67455b1
FallenStar <23637993+FallenStar08@users.noreply.github.com>
2023-12-05 03:44:04 +0100

    dumb shits

--- Fallen_Universal_MaxStack/Mods/Fallen_Universal_MaxStack/ScriptExtender/Lua/Server/main.lua
+++ Fallen_Universal_MaxStack/Mods/Fallen_Universal_MaxStack/ScriptExtender/Lua/Server/main.lua
@@ -2,18 +2,8 @@ Ext.Require("Shared/_Globals.lua")
 Ext.Require("Shared/_Utils.lua")
 _G.MaxStackAmount = 99999999
 
-
---Just In Case...
-Ext.Osiris.RegisterListener("TemplateAddedTo", 4, "after", function(root, item, inventoryHolder, addType)
-    if Osi.IsContainer(item) == 0 then
-        local entity = Ext.Entity.Get(item)
-        if entity.ServerItem then
-            entity.ServerItem.Item.Template.MaxStackAmount = _G.MaxStackAmount
-        end
-    end
-end)
-
-Ext.Osiris.RegisterListener("LevelGameplayStarted", 2, "after", function(level, isEditorMode)
+function MaxStackThings()
+    print("Doing")
     local startTime = Ext.Utils.MonotonicTime()
     local templates = Ext.Template.GetAllRootTemplates()
     local localTemplates = Ext.Template.GetAllLocalTemplates()
@@ -29,9 +19,24 @@ Ext.Osiris.RegisterListener("LevelGameplayStarted", 2, "after", function(level, 
     end
     local endTime = Ext.Utils.MonotonicTime()
     local elapsedTime = endTime - startTime
-    BasicPrint("Templates MaxStackAmount modified in " .. elapsedTime .. " milliseconds!",_,_,"Fall_MaxStack",true)
+    BasicPrint("Templates MaxStackAmount modified in " .. elapsedTime .. " milliseconds!", _, _, "Fall_MaxStack", true)
+end
+
+--Just In Case...
+Ext.Osiris.RegisterListener("TemplateAddedTo", 4, "after", function(root, item, inventoryHolder, addType)
+    if Osi.IsContainer(item) == 0 then
+        local entity = Ext.Entity.Get(item)
+        if entity.ServerItem then
+            entity.ServerItem.Item.Template.MaxStackAmount = _G.MaxStackAmount
+        end
+    end
 end)
 
+Ext.Events.GameStateChanged:Subscribe(function(e)
+    if e.FromState == "LoadSession" then
+        MaxStackThings()
+    end
+end)
 
-
-
+--Ext.Events.StatsLoaded:Subscribe(MaxStackThings)
+--Ext.Osiris.RegisterListener("LevelGameplayStarted", 2, "after", MaxStackThings)
