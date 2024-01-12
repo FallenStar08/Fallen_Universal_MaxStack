800ef2b614e4467db8fdc94073540511b259e73d
FallenStar <23637993+FallenStar08@users.noreply.github.com>
2024-01-12 06:56:31 +0100

    Improved logic and split patching into two parts for RT and LT

--- Fallen_Universal_MaxStack/Mods/Fallen_Universal_MaxStack/ScriptExtender/Lua/Server/main.lua
+++ Fallen_Universal_MaxStack/Mods/Fallen_Universal_MaxStack/ScriptExtender/Lua/Server/main.lua
@@ -1,36 +1,41 @@
 Ext.Require("Shared/_Globals.lua")
 Ext.Require("Shared/_Utils.lua")
-_G.MaxStackAmount = 99999999
+local MaxStackAmount = 99999999
 
-function MaxStackThings()
-    print("Doing")
+local function MaxStackLocalTemplates()
     local startTime = Ext.Utils.MonotonicTime()
-    local templates = Ext.Template.GetAllRootTemplates()
-    local localTemplates = Ext.Template.GetAllLocalTemplates()
-    for k, v in pairs(templates) do
-        if v.TemplateType == "item" and v.InventoryType == 0 then
-            v.MaxStackAmount = _G.MaxStackAmount
-        end
-    end
+    local localTemplates = Ext.Template.GetAllLocalTemplates() or {}
     for k, v in pairs(localTemplates) do
         if v.TemplateType == "item" and v.InventoryType == 0 then
-            v.MaxStackAmount = _G.MaxStackAmount
+            v.MaxStackAmount = MaxStackAmount
         end
     end
     local endTime = Ext.Utils.MonotonicTime()
     local elapsedTime = endTime - startTime
-    BasicPrint("Templates MaxStackAmount modified in " .. elapsedTime .. " milliseconds!", _, _, "Fall_MaxStack", true)
+    BasicPrint("Local Templates MaxStackAmount modified in " .. elapsedTime .. " milliseconds!", _, _, "Fall_MaxStack", true)
+end
+local function MaxStackRootTemplates()
+    local startTime = Ext.Utils.MonotonicTime()
+    local templates = Ext.Template.GetAllRootTemplates() or {}
+    for k, v in pairs(templates) do
+        if v.TemplateType == "item" and v.InventoryType == 0 then
+            v.MaxStackAmount = MaxStackAmount
+        end
+    end
+    local endTime = Ext.Utils.MonotonicTime()
+    local elapsedTime = endTime - startTime
+    BasicPrint("Root Templates MaxStackAmount modified in " .. elapsedTime .. " milliseconds!", _, _, "Fall_MaxStack", true)
+
 end
 
---Just In Case...
 Ext.Osiris.RegisterListener("TemplateAddedTo", 4, "after", function(root, item, inventoryHolder, addType)
     if Osi.IsContainer(item) == 0 then
         local entity = Ext.Entity.Get(item)
         if entity.ServerItem then
-            entity.ServerItem.Item.Template.MaxStackAmount = _G.MaxStackAmount
+            entity.ServerItem.Item.Template.MaxStackAmount = MaxStackAmount
         end
     end
 end)
 
---Ext.Events.StatsLoaded:Subscribe(MaxStackThings)
-Ext.Osiris.RegisterListener("LevelGameplayStarted", 2, "after", MaxStackThings)
+Ext.Events.SessionLoading:Subscribe(MaxStackRootTemplates)
+Ext.Osiris.RegisterListener("LevelGameplayStarted", 2, "after", MaxStackLocalTemplates)
